f11d6e2a43a7952707fc88d78e71f8ff98d936f1
Nhan12163 <phamphunhanpham8@gmail.com>
2024-09-10 19:59:54 +0700

    lab_3

---
+++ Dice App/dice/lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          centerTitle: true, // Sử dụng thuộc tính này để căn giữa tiêu đề
+          title: Text(
+            'Dice',
+          ),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 3;
+  int rightDiceNumber = 1;
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextButton(
+                child: Image.asset('images/dice_$leftDiceNumber.png'),
+                onPressed: () {
+                  changeDiceFace();
+                },
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextButton(
+                child: Image.asset('images/dice_$rightDiceNumber.png'),
+                onPressed: () {
+                  changeDiceFace();
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// class DicePage extends StatelessWidget {}
